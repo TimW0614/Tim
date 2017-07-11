39971cd29e21df29d8a3eefafb5f9229ab1f0acb
Augustina Wear <awear@cnm.edu>
2017-07-11 17:05:42 -0600

    adding to Tim about me

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 <html>
 	<head>
 		<link href="css/styles.css" rel="stylesheet">
-		<title></title>
+		<title>Tim</title>
 	</head>
 	<body>
-		<p>my name is tim williams. i am brand new to coding and am very excited for the road ahead.</p>
+		<p>My name is Tim Williams. I am brand new to coding and am very excited for the road ahead.
+			I was formally in the military and it was my pleasure to serve for you.</p>
 		<img src="images/timandmel.jpg"  alt="tim and mel"/>
-		<p> in the photo above we are at kona brewing in kona hawaii</p>
-		<p>my wife and i were married on june 14, 2016 in st.thomas usvi.</p>
+		<p> In the photo above I am visting Kona Brewing in Kona Hawaii.</p>
+		<p>My beautiful wife and I were married on June 14, 2016 in St.Thomas US Virgin Island</p>
 	</body>
+
