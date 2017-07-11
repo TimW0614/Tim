96dddf25126f566cb42496afe47fb41ca9c54d2f
Tim Williams <tikytimbo5@yahoo.com>
2017-07-10 19:35:16 -0600

    about tim day 1 assignment

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<title></title>
 	</head>
 	<body>
-		<p>My name is tim williams. i am brand new to coding and am very excited for the road ahead.</p>
+		<p>my name is tim williams. i am brand new to coding and am very excited for the road ahead.</p>
 		<img src="images/timandmel.jpg"  alt="tim and mel"/>
 		<p> in the photo above we are at kona brewing in kona hawaii</p>
 		<p>my wife and i were married on june 14, 2016 in st.thomas usvi.</p>
